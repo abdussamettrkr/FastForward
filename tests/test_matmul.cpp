d1785866f6ec03df2c03f4a91a1553791f7ef549
abdussamettrkr <abdussamettrkr@gmail.com>
2024-04-21 01:09:51 +0300

    update matmul tests

--- tests/test_matmul.cpp
+++ tests/test_matmul.cpp
@@ -2,26 +2,43 @@
 
 using namespace core;
 
+bool tensor_equal(const Tensor& t1, const Tensor& t2) {
+    if (t1.shape() != t2.shape()) return false;
+    for (size_t i = 0; i < t1.size(); ++i) {
+        if (std::abs(t1.data()[i] - t2.data()[i]) > 1e-6) return false; // tolerance for floating point comparison
+    }
+    return true;
+}
+
 bool test_matmul()
 {
-    Tensor left = Tensor::ones({3, 4});
-    Tensor right = Tensor::ones({4, 4});
-    left.data()[3] = 2;
-    left.data()[4] = 9;
-    left.data()[6] = 2.5;
-    left.data()[9] = 1.5;
+    
+    Tensor left1 = Tensor::ones({3, 4});
+    Tensor right1 = Tensor::ones({4, 4});
+    float expected1data[] = {4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4};
+    Tensor expected1({3, 4}, expected1data);
+    Tensor result1 = left1.matmul(right1);
+    if (tensor_equal(result1, expected1) != true)
+        return true;
 
-    right.data()[0] = 7;
-    right.data()[2] = 2.8;
-    right.data()[5] = 1.9;
-    right.data()[12] = 3.9;
-    right.data()[14] = 10.71;
+    Tensor left2 = Tensor::zeros({3, 4});
+    Tensor right2 = Tensor::ones({4, 4});
+    float expected2data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    Tensor expected2({3, 4}, expected2data);
+    Tensor result2 = left2.matmul(right2);
+    if (tensor_equal(result2, expected2) != true)
+        return true;
 
-    float arr[] = {16.8, 5.9, 26.22, 5, 70.4, 14.4, 39.41, 13.5, 13.4, 5.85, 16.01, 4.5};
-    Tensor expected({3, 4}, arr);
-    Tensor result = left.matmul(right);
-
-    return !(result == expected);
+    float left3data[] = {1, 2, 3, 4, 5, 6};
+    Tensor left3({2, 3}, left3data);
+    float right3data[] = {1, 2, 3, 4, 5, 6};
+    Tensor right3({3, 2}, right3data);
+    float expected3data[] = {22, 28, 49, 64};
+    Tensor expected3({2, 2}, expected3data);
+    Tensor result3 = left3.matmul(right3);
+    if (tensor_equal(result3, expected3) != true)
+        return true;
+    return false;
 }
 
 int main()
